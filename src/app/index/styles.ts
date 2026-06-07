ed8f6f080578d13a0455252c5a9fba9b3f495c88
ppedrolucas <pp15lucas@gmail.com>
2026-06-07 10:13:46 -0300

    Fundamentos do React Native;  Aula 6; Importações Flexíveis.

--- src/app/index/styles.ts
+++ src/app/index/styles.ts
@@ -1,5 +1,6 @@
+//Cominhos Relativos: O caminho relativo é o caminho que leva a um arquivo a partir do arquivo atual. Ele é representado por "./" para indicar o diretório atual e "../" para indicar o diretório pai. Por exemplo, se estamos no arquivo "src/app/index/index.tsx" e queremos importar algo do arquivo "src/app/index/styles.ts", podemos usar o caminho relativo "./styles".
+import { colors } from "@/styles/colors";
 import { StyleSheet } from "react-native";
-import { colors } from "../../styles/colors";
 
 //Para conseguirmos usar esse StyleSheet em outros arquivos, precisamos exportá-lo.
 export const styles = StyleSheet.create({
